01a418833090d2be35e55c6c9747b453f887be75
Queenu-7 <q.uwera@alustudent.com>
2025-06-20 07:18:02 -0700

    add

---
+++ python-if_else_loops_functions/10-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+add = __import__('10-add').add
+
+print(add(1, 2))        # Should print 3
+print(add(98, 0))       # Should print 98
+print(add(100, -2))     # Should print 98
