56d1abbdafb27ab77537404c5955982e6a87ee44
Queenu-7 <q.uwera@alustudent.com>
2025-06-20 10:10:15 -0700

    replace

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    # Return a new list where each element is replaced if it matches 'search'
+    return [replace if x == search else x for x in my_list]
