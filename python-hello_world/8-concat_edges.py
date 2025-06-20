7e86c678720cee9de35bcbcd6ee547f89d20e0a0
Queenu-7 <q.uwera@alustudent.com>
2025-06-20 05:45:57 -0700

    concat edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
-str = ("Python is an interpreted, interactive, object-oriented "
-       "programming language that combines remarkable power with very clear syntax")
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+#!/usr/bin/env python3
+str1 = "object-oriented programming"
+str2 = " with Python"
+# The next two lines are your solution
+str1 += str2
+print(str1)
