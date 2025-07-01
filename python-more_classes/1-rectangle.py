e8b75fdad561dfaebc3eb2d4378bf1a54c424e61
Queenu-7 <q.uwera@alustudent.com>
2025-07-01 07:49:43 -0700

    1st task

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module defines a Rectangle class with validated width and height attributes.
+This module defines a Rectangle class with
+validated width and height attributes.
 """
 
 
