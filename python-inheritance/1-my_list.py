b924857475009e1f91df9a90bfa80532493e4234
Queenu-7 <q.uwera@alustudent.com>
2025-07-02 06:00:52 -0700

    testing

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""
+Module 1-my_list
+Defines a MyList class that inherits from list and adds a print_sorted method.
+"""
+
 
 class MyList(list):
+    """
+    MyList inherits from the built-in list.
+    
+    Public instance method:
+        print_sorted(self): Prints the list sorted in ascending order.
+    """
+
     def print_sorted(self):
+        """Prints the list sorted in ascending order."""
         print(sorted(self))
