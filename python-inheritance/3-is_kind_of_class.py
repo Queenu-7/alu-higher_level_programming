be3d5ee7f566810e9a42796a04963adc2ec0635f
Queenu-7 <q.uwera@alustudent.com>
2025-07-02 06:23:15 -0700

    kind

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines is_kind_of_class function."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if obj is instance of or inherits from a_class."""
+    return isinstance(obj, a_class)
