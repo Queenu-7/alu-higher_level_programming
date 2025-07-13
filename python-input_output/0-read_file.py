de2405d3d170736d9683876597c15147ef3f66de
Queenu-7 <q.uwera@alustudent.com>
2025-07-13 08:19:14 -0700

    read

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,7 @@ Module: 0-read_file
 Reads a UTF-8 encoded text file and prints its contents to stdout.
 """
 
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
