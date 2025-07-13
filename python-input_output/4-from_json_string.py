277189d73f7ca6bb538b11ce61f038292013e517
Queenu-7 <q.uwera@alustudent.com>
2025-07-13 08:29:27 -0700

    object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module: 4-from_json_string
+Returns a Python object represented by a JSON string.
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Converts a JSON string to a Python object.
+    """
+    return json.loads(my_str)
