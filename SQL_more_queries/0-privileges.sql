091a79ce31bbf2ec6b8c45557c08ecb75adef934
Queenu-7 <q.uwera@alustudent.com>
2025-07-17 11:17:04 -0700

    priviledges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,13 +1,3 @@
--- List privileges for user_0d_1 only if the user exists
-SELECT 'Grants for user_0d_1@localhost' AS info;
-SELECT 
-    IF(COUNT(*) = 0, 'User user_0d_1@localhost does not exist', '') AS message 
-FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost';
+-- List privileges for the user existence
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List privileges for user_0d_2 only if the user exists
-SELECT 'Grants for user_0d_2@localhost' AS info;
-SELECT 
-    IF(COUNT(*) = 0, 'User user_0d_2@localhost does not exist', '') AS message 
-FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
