5f964013b2d5323f4ece5e7dd8fe1321e362ae29
Queenu-7 <q.uwera@alustudent.com>
2025-07-21 02:49:31 -0700

    body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Sends a GET request to the given URL and displays the size of the response body in bytes
-curl -s "$1" -o temp_response && wc -c < temp_response && rm -f temp_response
+#!/bin/bash 
+# take in a url and display the content 
+curl -sI "$1" | grep 'Content-Length'| cut -d " " -f2
