54edb9fe4a0055ef6892069d5a0ed29713573c37
Queenu-7 <q.uwera@alustudent.com>
2025-07-26 06:51:41 -0700

    email

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """
-Module 2-post_email.py
+This module sends a POST request to a URL with an email parameter and displays
+the body of the response decoded in UTF-8 format.
 """
 
-
-import urllib.parse
 import urllib.request
-from sys import argv
+import urllib.parse
+import sys
 
 
 if __name__ == "__main__":
-    url = argv[1]
-    value = {"email": argv[2]}
-    data = urllib.parse.urlencode(value)
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email})
     data = data.encode('ascii')
+
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        page = response.read().decode('utf-8')
-        print(page)
+        body = response.read()
+        print(body.decode('utf-8'))
