9172e77e9cc079a6114307d470efda43f4f06015
Queenu-7 <q.uwera@alustudent.com>
2025-07-26 08:06:34 -0700

    post email

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """
-Module 6-post_email.py
+This module sends a POST request to a URL with an email parameter using the
+requests package and displays the body of the response.
 """
 
-
 import requests
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    resp = requests.post(argv[1], data={"email": argv[2]})
-    print(resp.text)
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    print(response.text)
