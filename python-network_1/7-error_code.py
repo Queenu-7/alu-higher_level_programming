b8cc46799505a81634c7f06461cfc922d543910f
Queenu-7 <q.uwera@alustudent.com>
2025-07-26 08:07:06 -0700

    2nd error code

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """
-Module 7-error_code.py
+This module sends a request to a URL using the requests package and displays
+the body of the response. If the HTTP status code is >= 400, it displays
+the error code instead.
 """
 
-
 import requests
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    resp = requests.get(argv[1])
-    if resp.status_code >= 400:
-        print("Error code: {}".format(resp.status_code))
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
     else:
-        print(resp.text)
+        print(response.text)
