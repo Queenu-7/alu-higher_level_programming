9d3a45a3c46d8b95662edec40e0111238075b13d
Queenu-7 <q.uwera@alustudent.com>
2025-10-05 10:31:42 -0700

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,6 +2,8 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], (error, paragraph) => {
-	  if (error) throw error;
+	  if (error) {
+		      throw error;
+		    }
 	  console.log(paragraph.toString());
 });
