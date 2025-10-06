4ae419f9b3651e33fd724fd7311dc9a3a68cd6d1
Queenu-7 <q.uwera@alustudent.com>
2025-10-06 06:05:29 -0700

    task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -16,22 +16,4 @@ request.get(url, (err, response, body) => {
 			            console.log(err);
 			          }
 		    });
-});#!/usr/bin/node
-const request = require('request');
-const fs = require('fs');
-
-const url = process.argv[2]; // first argument → URL
-const filePath = process.argv[3]; // second argument → file path
-
-request.get(url, (err, response, body) => {
-	  if (err) {
-		      console.log(err);
-		      return;
-		    }
-
-	  fs.writeFile(filePath, body, 'utf-8', (err) => {
-		      if (err) {
-			            console.log(err);
-			          }
-		    });
 });
