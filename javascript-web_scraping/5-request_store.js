23630d2ea082913682429cd3dc56a5880eb55ab5
Queenu-7 <q.uwera@alustudent.com>
2025-10-06 06:26:04 -0700

    task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,18 +2,19 @@
 const request = require('request');
 const fs = require('fs');
 
-const url = process.argv[2]; // first argument → URL
-const filePath = process.argv[3]; // second argument → file path
+
+const url = process.argv[2]; //first argument url
+const filepath = process.argv[3]; // second argument file path
 
 request.get(url, (err, response, body) => {
-	  if (err) {
-		      console.log(err);
-		      return;
-		    }
+  if (err) {
+    console.log(err);
+    return;
+	}
 
-	  fs.writeFile(filePath, body, 'utf-8', (err) => {
-		      if (err) {
-			            console.log(err);
-			          }
-		    });
-});
+
+  fs.writeFile(filepath, body, 'utf-8', (err) => {
+    if (err) {
+      console.log(err);
+	  }
+	});
